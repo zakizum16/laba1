8cff0e15a8cb0fe2f6716c4f5c56d3476f3f1ca0
Рогоньян Роман <dreamsteam090@gmail.com>
2025-10-08 10:43:29 +0300

    aaaaaa

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 
-void main() {
-
+public class Main {
+    public static void main() {
+        int x = 4;
+        int i =0;
+        for (i=0;i<1000;i++ ){
+            System.out.println(i + " - номер");
+        }
+        System.out.println(x);
+    }
 }
